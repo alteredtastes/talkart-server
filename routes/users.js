ef7dcfbadd535989e0bb9db05f29d5cf9d8a74c3
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-06-29 15:54:42 -0700

    returns a jwt for valid user password lookups

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
+var jwt = require('jsonwebtoken');
 
+require('dotenv').load();
 /* GET users listing. */
 
 router.get('/', (req, res, next) => {
@@ -19,13 +21,18 @@ router.post('/login', (req, res, next) => {
     .where({username: req.body.username})
     .then(function(data) {
       if(data[0].password === req.body.password) {
-        res.send('login successful.');
+        console.log(data[0]);
+        var token = jwt.sign(data[0], process.env.APP_SECRET, {
+          expiresIn: '1d'
+        }
+    );
+        res.json({success: true, message: 'Here is your token.', token: token});
       } else {
-        res.send('wrong password!');
+        res.json({success: false, message: 'wrong password!'});
       }
     })
     .catch(function(err) {
-      res.send('error!');
+      res.json({success:false, message: err});
     });
 });
 
